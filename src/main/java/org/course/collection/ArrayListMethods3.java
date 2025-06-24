8c78af2d7dc5636265ee021f424a1d6e426f8305
iakon <iakonien@gmail.com>
2025-06-24 21:34:51 +0300

    Методы ArrayList 2

--- src/main/java/org/course/collection/ArrayListMethods3.java
+++ src/main/java/org/course/collection/ArrayListMethods3.java
@@ -9,6 +9,7 @@ public class ArrayListMethods3 {
         arrayList1.add("Zaur");
         arrayList1.add("Ivan");
         arrayList1.add("Mariya");
+        arrayList1.add("Ivan");
 
         ArrayList<String> arrayList2 = new ArrayList<>();
         arrayList2.add("Lesha");
@@ -25,14 +26,27 @@ public class ArrayListMethods3 {
         arrayList1.addAll(0, arrayList3);
 
         System.out.println(arrayList1);
+        /* System.out.println(arrayList1.toString()); */
 
         System.out.println(arrayList1.get(arrayList1.indexOf("Ivan")));
 
+        System.out.println(arrayList1.lastIndexOf("Ivan"));
 
+        System.out.println(arrayList1.size());
+
+        System.out.println(arrayList1.isEmpty());
+
+        System.out.println("То чего нет в листе " + arrayList1.contains("sadsa"));
+        System.out.println("Иван есть в листе? " + arrayList1.contains("Ivan"));
+
+        /// ///////////////////////////
 
         arrayList1.clear();
 
         System.out.println(arrayList1);
+
+        System.out.println(arrayList1.isEmpty());
+
 
 
     }
