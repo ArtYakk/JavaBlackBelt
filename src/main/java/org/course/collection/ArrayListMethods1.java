b2b1c49d21c581a5d329afc53daae1ea6a793f0c
Artem <iakonien@gmail.com>
2025-06-24 15:02:26 +0300

    Методы ArrayList 1, equals

---
+++ src/main/java/org/course/collection/ArrayListMethods1.java
@@ -0,0 +1,53 @@
+package org.course.collection;
+
+import java.util.ArrayList;
+
+public class ArrayListMethods1 {
+    public static void main(String[] args) {
+        ArrayList<String> arrayList1 = new ArrayList<>();
+        arrayList1.add("Zaur");
+        arrayList1.add("Ivan");
+        arrayList1.add("Mariya");
+        arrayList1.add(1, "Misha");
+
+        /* arrayList1.add(9, "Misha");
+        * IndexOutOfBoundsException
+        * Размер массива = 4
+        * */
+
+        for(String s : arrayList1){
+            System.out.print(s + " ");
+        }
+
+        System.out.println();
+
+        /*ArrayList<int> arrayList2 = new ArrayList<>();
+        * Нельзя юзать примитивы, у каждого примитива есть
+        * wrapper-класс, чтобы переменная была ссылочного типа
+        * */
+
+        System.out.print("Метод get: ");
+        for (int i = 0; i < arrayList1.size(); i++) {
+            System.out.print(arrayList1.get(i) + " ");
+        }
+        System.out.println();
+
+        System.out.println(arrayList1.get(1));
+
+        arrayList1.set(1, "MASHA");
+        System.out.println(arrayList1);
+
+        arrayList1.remove(1);
+        System.out.println(arrayList1);
+
+        try {
+            arrayList1.remove(10);
+        }catch (IndexOutOfBoundsException e){
+            System.out.println(e);
+        }
+
+
+
+
+    }
+}
---
+++ src/main/java/org/course/collection/ArrayListMethods2.java
@@ -0,0 +1,82 @@
+package org.course.collection;
+
+import java.util.ArrayList;
+import java.util.Objects;
+
+public class ArrayListMethods2 {
+    public static void main(String[] args) {
+        Student student1 = new Student("Artem", 'm', 23, 4, 5.0);
+        Student student2 = new Student("Ivan", 'm', 34, 3, 4.0);
+        Student student3 = new Student("Nikolay", 'm', 45, 2, 3.9);
+        Student student4 = new Student("Elena", 'f', 56, 3, 5.0);
+        Student student5 = new Student("Mariya", 'f', 17, 1, 4.7);
+
+        ArrayList<Student> studentList = new ArrayList<>();
+        studentList.add(student1);
+        studentList.add(student2);
+        studentList.add(student3);
+        studentList.add(student4);
+        studentList.add(student5);
+        System.out.println(studentList);
+
+        Student student6 = new Student("Mariya", 'f', 17, 1, 4.7);
+        studentList.remove(student6);
+        System.out.println(studentList);
+    }
+}
+
+class Student{
+    private String name;
+    private char sex;
+    private int age;
+    private int course;
+    private double avgGrade;
+
+    public Student(String name, char sex, int age, int course, double avgGrade) {
+        this.name = name;
+        this.sex = sex;
+        this.age = age;
+        this.course = course;
+        this.avgGrade = avgGrade;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "name='" + name + '\'' +
+                ", sex='" + sex + '\'' +
+                ", age='" + age + '\'' +
+                ", course='" + course + '\'' +
+                ", avgGrade='" + avgGrade + '\'' +
+                '}';
+    }
+
+    /*@Override
+    public boolean equals(Object obj) {
+        if(!this.getClass().equals(obj.getClass())) return false;
+
+        if(!this.name.equals(((Student) obj).name)) return false;
+        if(!(this.sex == ((Student) obj).sex)) return false;
+        if(!(this.age == ((Student) obj).age)) return false;
+        if(!(this.course == ((Student) obj).course)) return false;
+        if(!(this.avgGrade == ((Student) obj).avgGrade)) return false;
+
+        return true;
+    }*/
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Student student = (Student) o;
+        return sex == student.sex &&
+                age == student.age &&
+                course == student.course &&
+                Double.compare(avgGrade, student.avgGrade) == 0 &&
+                Objects.equals(name, student.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, sex, age, course, avgGrade);
+    }
+}
