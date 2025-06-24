ce552a4474b1bfaf2ad9362c7452059a7d1d3194
Artem <iakonien@gmail.com>
2025-06-24 14:24:40 +0300

    ArrayList1

---
+++ src/main/java/org/course/collection/ArrayListEx1.java
@@ -0,0 +1,33 @@
+package org.course.collection;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ArrayListEx1 {
+    public static void main(String[] args) {
+        ArrayList/*<String>*/ arrayList1 = new ArrayList<>();
+        arrayList1.add("Zaur");
+        arrayList1.add("Ivan");
+        arrayList1.add("Mariya");
+        arrayList1.add("Dima");
+        arrayList1.add(2);
+        arrayList1.add(new Car());
+
+        System.out.println(arrayList1);
+
+        ArrayList<String> arrayList2 = new ArrayList<>(30);
+        for (int i=0; i<30; i++){
+            arrayList2.add("Item " + i);
+        }
+        System.out.println(arrayList2);
+
+        List<String> arrayList3 = new ArrayList<>();
+
+        List<String> arrayList4 = new ArrayList<>(arrayList1);
+        System.out.println(arrayList4);
+
+        System.out.println(arrayList1 == arrayList4);
+    }
+}
+
+class Car{}
