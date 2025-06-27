941f99fc79ebe21a7d85bb41710e513aae4cac09
Artem <iakonien@gmail.com>
2025-06-27 15:53:57 +0300

    HashSet<T>

---
+++ src/main/java/org/course/collection/HashSetEx.java
@@ -0,0 +1,24 @@
+package org.course.collection;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class HashSetEx {
+    public static void main(String[] args) {
+        Set<String> set = new HashSet<>();
+        set.add("Zaur");
+        set.add("Oleg");
+        set.add("Marina");
+        set.add("Igor");
+        //set.add(null);
+
+        System.out.println(set);
+        set.forEach(System.out::println);
+
+        set.remove("Zaur");
+        System.out.println(set);
+        System.out.println(set.contains("Marina"));
+        
+
+    }
+}
