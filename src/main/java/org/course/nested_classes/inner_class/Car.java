15919b85ebfd22f51dfbc1aaef6ea754b7e9c98c
Artem <iakonien@gmail.com>
2025-06-30 14:57:02 +0300

    Inner class

--- src/main/java/org/course/nested_classes/inner_class/Car.java
+++ src/main/java/org/course/nested_classes/inner_class/Car.java
@@ -6,17 +6,15 @@ public class Car {
     int doorCount;
     Engine engine;
 
-    public Car(String color, int doorCount, int horsePower) {
+    public Car(String color, int doorCount/*, int horsePower*/) {
         this.color = color;
         this.doorCount = doorCount;
-        this.engine = this.new Engine(horsePower);
+//        this.engine = this.new Engine(horsePower);
     }
 
-//    void method(){
-//        System.out.println(Car.Engine.countOfObjects);
-//        Car.Engine e = new Car.Engine(240);
-//        System.out.println(e.horsePower);
-//    }
+    public void setEngine(Engine engine){
+        this.engine = engine;
+    }
 
     @Override
     public String toString() {
@@ -29,10 +27,14 @@ public class Car {
 
     public class Engine /*extends Z*/ {
         private int horsePower;
-
+        public static int a;
+        public static final int b = 25;
 
         public Engine(int horsePower) {
             this.horsePower = horsePower;
+            /*System.out.println(doorCount); Можно спокойно обращаться к полям outer класса,
+            даже к приватным
+            * */
         }
 
         @Override
@@ -46,6 +48,14 @@ public class Car {
 
 class Test{
     public static void main(String[] args) {
-        Car car = new Car("Black", 5, )
+
+        Car car = new Car("Black", 5);
+        Car.Engine engine = car.new Engine(200);
+
+        /*Car.Engine engine1 = new Car("Black", 5).new Engine(140);*/
+
+        car.setEngine(engine);
+        System.out.println(engine);
+        System.out.println(car);
     }
 }
