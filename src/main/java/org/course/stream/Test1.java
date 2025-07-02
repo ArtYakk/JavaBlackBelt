c06dc838993910885b5bd37cd48b8c6412cf22b7
Artem <iakonien@gmail.com>
2025-07-02 09:55:56 +0300

    Stream, map(), collect(), Collectors

--- src/main/java/org/course/stream/Test1.java
+++ src/main/java/org/course/stream/Test1.java
@@ -1,8 +1,8 @@
 package org.course.stream;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Test1 {
     public static void main(String[] args) {
@@ -12,13 +12,39 @@ public class Test1 {
         list.add("ok");
         list.add("poka");
 
-        for(int i=0; i<list.size(); i++){
+      /*  for(int i=0; i<list.size(); i++){
             list.set(i, String.valueOf(list.get(i).length()));
-        }
+        }*/
         System.out.println(list);
 
+        List<Integer> list2 = list.stream().map(a -> a.length()).collect(Collectors.toList());
+                                                                            /* .toList()*/
         System.out.println(list.stream().map(a -> a + " " + a.length()).toList());
 
+        System.out.println(list2);
 
+        int[] array = {5,9,3,8,1};
+       int[] res = Arrays.stream(array)
+               .map(a -> {
+                   if (a % 3 == 0){
+                       a /= 3;
+                   }
+                   return a;
+               })
+               .toArray();
+        System.out.println(Arrays.toString(res));
+        System.out.println("------------------------------------------------------------------------------\n" +
+                "Работа с Set");
+
+        Set<String> set = new TreeSet<>();
+        set.add("privet");
+        set.add("kak dela?");
+        set.add("ok");
+        set.add("poka");
+        System.out.println(set);
+        Set<Integer> listFromSet = set.stream()
+                .map(a -> a.length())
+                .collect(Collectors.toSet());
+        System.out.println(listFromSet);
     }
 }
