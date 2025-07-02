010b6c951978186386b0ea5e8cc7dc98fe16857c
iakon <iakonien@gmail.com>
2025-07-02 22:25:40 +0300

    mapToInt(), IntStream mapToInt() возвращает поток примитивов IntStream, для того чтобы работать с ним дальше как с обычным стримом, надо обернуть его методом boxed() Методы IntStream: sum(), average(), min(), max()

--- src/main/java/org/course/stream/Test9.java
+++ src/main/java/org/course/stream/Test9.java
@@ -2,6 +2,7 @@ package org.course.stream;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Test9 {
     public static void main(String[] args) {
@@ -17,6 +18,34 @@ public class Test9 {
         students.add(st4);
         students.add(st5);
 
+        List<Integer> courses = students.stream()
+                .mapToInt(a -> a.getCourse())
+                .boxed()
+                .collect(Collectors.toList());
+        System.out.println(courses);
+
+        int sum = students.stream()
+                .mapToInt(a -> a.getCourse())
+                .sum();
+        System.out.println("Sum = " + sum);
+
+        double average = students.stream()
+                .mapToInt(a -> a.getCourse())
+                .average().getAsDouble();
+
+        System.out.println("Average = " + average);
+
+        int min = students.stream()
+                .mapToInt(a -> a.getCourse())
+                .min().getAsInt();
+        System.out.println("Min = " + min);
+
+        int max = students.stream()
+                .mapToInt(a -> a.getCourse())
+                .max().getAsInt();
+        System.out.println("Max = " + max);
+
+
 //       Student min =  students.stream()
 //               .min((a,b) -> a.getAge() - b.getAge())
 //               .get();
@@ -26,23 +55,23 @@ public class Test9 {
 //                .max((a,b) -> a.getAge() - b.getAge())
 //                .get();
 //        System.out.println(max);
-        students.stream()
-                .filter(a -> a.getAge() > 20)
-                .forEach(System.out::println);
-
-        System.out.println("-------------limit-2------------------------------------------");
-
-        students.stream()
-                .filter(a -> a.getAge() > 20)
-                .limit(2)
-                .forEach(System.out::println);
-
-        System.out.println("--------------skip-2-----------------------------------------");
-
-        students.stream()
-                .filter(a -> a.getAge() > 20)
-                .skip(2)
-                .forEach(System.out::println);
+//        students.stream()
+//                .filter(a -> a.getAge() > 20)
+//                .forEach(System.out::println);
+//
+//        System.out.println("-------------limit-2------------------------------------------");
+//
+//        students.stream()
+//                .filter(a -> a.getAge() > 20)
+//                .limit(2)
+//                .forEach(System.out::println);
+//
+//        System.out.println("--------------skip-2-----------------------------------------");
+//
+//        students.stream()
+//                .filter(a -> a.getAge() > 20)
+//                .skip(2)
+//                .forEach(System.out::println);
 
     }
 }
