afe72670780a5f1610e8c0378367c1c707e1d5f3
iakon <iakonien@gmail.com>
2025-07-02 21:15:42 +0300

    concat(stream1, stream2, ...)

--- src/main/java/org/course/stream/Test6.java
+++ src/main/java/org/course/stream/Test6.java
@@ -12,5 +12,11 @@ public class Test6 {
 //                    return a%2 == 0;
 //                })
 //                .toList();
+        Stream<Integer> stream2 = Stream.of(1,2,3,4,5);
+        Stream<Integer> stream3 = Stream.of(6, 7, 8, 9, 10);
+        Stream<Integer> stream4 = Stream.concat(stream2, stream3);
+        stream4.forEach(System.out::println);
+
+
     }
 }
