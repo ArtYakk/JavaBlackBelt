bce62348d294f122b805e20bf1c7d05a1ce5aa46
iakon <iakonien@gmail.com>
2025-07-08 22:29:03 +0300

    ArrayBlockingQueue. Блокирующая очередь,по принципу FIFO. один поток добавляет, второй забирает из начала очереди, если там есть элемент

---
+++ src/main/java/org/course/thread_safe/ArrayBlockingQueueEx2.java
@@ -0,0 +1,35 @@
+package org.course.thread_safe;
+
+import java.util.concurrent.ArrayBlockingQueue;
+
+public class ArrayBlockingQueueEx2 {
+    public static void main(String[] args) {
+        ArrayBlockingQueue<Integer> queue = new ArrayBlockingQueue<>(4);
+        //Producer
+        new Thread(() -> {
+            int i = 0;
+            while (true){
+                try {
+                    queue.put(++i);
+                    System.out.println("Producer added num: " + i + " " + queue);
+                    Thread.sleep(3000);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        }).start();
+
+        //Consumer
+        new Thread(() -> {
+            while (true){
+                try {
+                    Integer j = queue.take();
+                    System.out.println("Consumer grabbed value:  " + j + " " + queue);
+                    Thread.sleep(9000);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        }).start();
+    }
+}
